ca8b347a406e0bf4ded6ddeb6b2f84b287e2498e
PreethikhaS <preethikhasundarabharathy@gmail.com>
2025-01-15 20:47:38 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavMenu from "./components/NavMenu"
+import './App.css';
+
+function App() {
+  return (
+ <main>
+    <NavMenu/>
+ </main>
+  );
+}
+
+export default App;
